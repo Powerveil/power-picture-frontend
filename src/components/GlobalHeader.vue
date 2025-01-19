c688a854e28108f3786bd0d5dce6cc0676fae182
Powerveil <zcuishuai@yeah.net>
2025-01-20 02:45:07 +0800

    初始化项目

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,100 @@
+<template>
+
+  <div id="globalHeader">
+    <a-row :wrap="false">
+      <a-col flex="200px">
+        <router-link to="/">
+          <div class="title-bar">
+            <img class="logo" src="../assets/logo.ico" alt="logo"></img>
+            <div class="title">power云图库</div>
+          </div>
+        </router-link>
+      </a-col>
+      <a-col flex="auto">
+        <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" @click="doMenuClick"/>
+      </a-col>
+
+      <a-col flex="120px">
+        <div class="user-login-status">
+          <div v-if="loginUserStore.loginUser.id">
+            {{ loginUserStore.loginUser.userName ?? '无名'}}
+          </div>
+          <div v-else>
+            <a-button type="primary" href="/user/login">登录</a-button>
+          </div>
+        </div>
+      </a-col>
+    </a-row>
+
+
+
+
+
+
+  </div>
+
+</template>
+<script lang="ts" setup>
+import { h, ref } from 'vue'
+import { HomeOutlined } from '@ant-design/icons-vue'
+import { MenuProps } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
+import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
+
+
+const loginUserStore = useLoginUserStore()
+
+const items = ref<MenuProps['items']>([
+  {
+    key: '/',
+    icon: () => h(HomeOutlined),
+    label: '主页',
+    title: '主页'
+  },
+  {
+    key: '/about',
+    label: '关于',
+    title: '关于'
+  },
+  {
+    key: 'others',
+    label: h('a', { href: 'http://81.70.86.227:8787', target: '_blank' }, '进入地球Online的男人'),
+    title: '进入地球Online的男人'
+  }
+])
+
+
+const router = useRouter()
+
+const doMenuClick = ({ key }) => {
+  router.push({
+    path: key
+  })
+}
+
+const current = ref<string[]>([])
+router.afterEach((to, from, next) => {
+  current.value = [to.path]
+})
+
+</script>
+
+
+<style scoped>
+#globalHeader .title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: black;
+  font-size: 18px;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+}
+
+</style>
+
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="homePage">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+const msg = "dsadas"
+</script>
+
+<style  scoped>
+#homePage {
+
+}
+</style>
+
+<!--<template>-->
+
+
+<!--  <a-config-provider :locale="locale">-->
+<!--    <div class="homePage">-->
+<!--      <h1>{{msg}}</h1>-->
+<!--      <a-button type="primary">Primary</a-button>-->
+<!--    </div>-->
+<!--  </a-config-provider>-->
+<!--</template>-->
+
+
+<!--<script setup lang="ts">-->
+<!--const msg = "dsadas"-->
+
+<!--import locale from 'ant-design-vue/es/locale/zh_CN';-->
+<!--import dayjs from 'dayjs';-->
+<!--import 'dayjs/locale/zh-cn';-->
+<!--dayjs.locale('zh-cn');-->
+
+<!--</script>-->
+
+<!--<style  scoped>-->
+<!--#homePage {-->
+
+<!--}-->
+<!--</style>-->
